c05b4c09a7be5a421decbaec90da60cff52850e1
justin <justinlin@ufl.edu>
2024-10-26 16:16:42 -0400

    Add signup functionality

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -3,19 +3,21 @@
 import { ReactNode } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
-export const Navbar = ({children}: {children?: ReactNode}) => {
+export const Navbar = ({ children }: { children?: ReactNode }) => {
+  const navigate = useNavigate();
+  
   return (
-    <nav className="bg-slate-400 h-[60px] w-full flex justify-center fixed top-0 left-0 z-[49]">
+    <nav className="bg-primary-foreground h-[60px] w-full flex justify-center fixed top-0 left-0 z-[49]">
       <ul className="w-[70%] h-full flex items-center gap-4">
         <Link className="mr-auto border-none" to="/home">
           [LOGO IMG]
         </Link>
         <Link
-          className="flex items-center justify-center h-[calc(60px*0.6)] w-[calc(60px*0.6)] 
+          className="flex items-center justify-center h-[calc(60px*0.6)] w-[calc(60px*0.6)] text-background
         border-none rounded-full transition-all duration-150 ease-linear bg-white cursor-pointer hover:brightness-75"
-          to="/gemi-ai"
+          to="/signup"
         >
-            D/R
+          D/R
         </Link>
         {children}
       </ul>
