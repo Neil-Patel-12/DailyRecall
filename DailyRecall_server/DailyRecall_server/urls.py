984f85fde841454f68b5382a4a6bd738497c7d89
ethan <ethan.lee.zhu@gmail.com>
2024-10-31 17:12:02 -0400

    update requirements and urls

--- DailyRecall_server/DailyRecall_server/urls.py
+++ DailyRecall_server/DailyRecall_server/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from api.views import CreateUserView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/user/register/", CreateUserView.as_view(), name="register"),
@@ -26,4 +27,5 @@ urlpatterns = [
     path("api/token/refresh/", TokenRefreshView.as_view(), name="refresh"),
     path("api-auth/", include("rest_framework.urls")),
     path("api/", include("api.urls")),
+
 ]
--- DailyRecall_server/api/urls.py
+++ DailyRecall_server/api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LoginView
+
 
 urlpatterns = [
     path("user_post/", views.User_PostListCreate.as_view(), name="post_list"),
     path("user_post/delete/<int:pk>", views.User_PostDelete.as_view(), name="delete_post"),
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),    # may be relocated to /api path
 ]
