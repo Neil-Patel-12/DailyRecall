f4a96bb745fe8930bb9e3f2fd26e5f107a845f7b
Neil Patel <neilpatel235@gmail.com>
2024-12-05 16:26:16 -0500

    refactored database tables

--- DailyRecall_server/api/admin.py
+++ DailyRecall_server/api/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
-from .models import User_Post
+# # Register your models here.
+# from .models import User_Post
 
 
-# admin.site.register(User_Post)
+# # admin.site.register(User_Post)
 
 
-@admin.register(User_Post)
-class User_PostAdmin(admin.ModelAdmin):
-    list_display = ("subject", "topic", "author", "mastery")
-    list_filter = ("subject", "topic")
+# @admin.register(User_Post)
+# class User_PostAdmin(admin.ModelAdmin):
+#     list_display = ("subject", "topic", "author", "mastery")
+#     list_filter = ("subject", "topic")
--- DailyRecall_server/api/models.py
+++ DailyRecall_server/api/models.py
@@ -5,27 +5,35 @@ from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 from django.core.validators import MinValueValidator, MaxValueValidator
 
+class Topic(models.Model):
+    name = models.CharField(max_length=50, blank=False)
+    SUBJECT_ITEMS = (
+        ('m', 'Math'),
+        ('s', 'Science'),
+        ('a', 'Art'),
+        ('h', 'History'),
+        ('x', "Misc"),
+    )
+    subject = models.CharField(
+        max_length=1,
+        choices=SUBJECT_ITEMS,
+        default='x',
+        help_text="choices one subject")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="topics")
+    
+    def __str__(self):
+        return f"{self.author.username} - {self.subject} - {self.name}"
 
 # Create your models here.
 class User_Post(models.Model):
-    subject = models.CharField(
-        max_length=255
-    )  # What broad area was learned ('Math', 'Programming')
-    topic = models.CharField(max_length=255)  # More specific ('Algebra', 'React.js')
-    mastery = models.IntegerField(
-        validators=[MinValueValidator(1), MaxValueValidator(5)]
-    )
-    # A rating (from 1 to 5) that reflects how well they feel they understand the topic
-    content = (
-        models.TextField()
-    )  # Optional: a more detailed explanation of what was learned
+    title = models.CharField(max_length=50, null=True, blank=False)
+    confidence = models.IntegerField(default=5, validators=[MinValueValidator(1), MaxValueValidator(10)])
+    content = models.TextField()  # Optional: a more detailed explanation of what was learned
 
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="user_post"
-    )  # Link the post to a user
-    date_posted = models.DateField(
-        auto_now_add=True
-    )  # Automatically store the date the post was made
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="post")
+    date_posted = models.DateField(auto_now_add=True)
+
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE, related_name="post")
 
     def __str__(self):
-        return f"{self.author.username} - {self.subject} ({self.date_posted})"
+        return f"{self.author.username} - {self.title} ({self.date_posted})"
